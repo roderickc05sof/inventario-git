1a83a21112782da2914ced020654035e3f88f500
Roderick Castillo <roderickcastillo3000@gmail.com>
2026-07-06 09:16:33 -0500

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Teams\TeamInvitationController;
+use App\Http\Middleware\EnsureTeamMembership;
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'inicio')->name('home');
+
+
+
+
+Route::prefix('{current_team}')
+    ->middleware(['auth', 'verified', EnsureTeamMembership::class])
+    ->group(function () {
+        Route::get('dashboard', DashboardController::class)->name('dashboard');
+    });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('invitations/{invitation}/accept', [TeamInvitationController::class, 'accept'])->name('invitations.accept');
+    Route::delete('invitations/{invitation}', [TeamInvitationController::class, 'decline'])->name('invitations.decline');
+});
+
+
+
+require __DIR__.'/settings.php';
