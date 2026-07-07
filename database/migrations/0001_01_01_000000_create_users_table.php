ba5a372276944a8c833dc3fde15709501e7690ea
roderickc05sof <roderickc05@icloud.com>
2026-07-07 09:56:08 -0500

    agregar tablas sin depencias y parametros a tabla user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('id_role')->constrained('roles','id_role');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
